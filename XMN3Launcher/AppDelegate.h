0cb972bf8a238da27bc4ab628528f4427abf7da4
lugeek <me@lugeek.com>
2020-09-24 15:23:53 +0800

    Initial Commit

---
+++ XMN3Launcher/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  XMN3Launcher
+//
+//  Created by lugeek on 2020/9/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ XMN3Launcher/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  XMN3Launcher
+//
+//  Created by lugeek on 2020/9/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
